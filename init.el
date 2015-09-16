507685307eea6db4d73db8f8033e23ead3aa125b
Jason Foreman <jason@threeve.org>
2015-09-15 22:15:09 -0500

    Strong are you with the use-package.

--- init.el
+++ init.el
@@ -4,5 +4,18 @@
 
 ;;; Code:
 
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
 (provide 'init)
 ;;; init.el ends here
