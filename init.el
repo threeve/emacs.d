21f7f1281fa78b13dad134bc90aadfa4c603e46d
Jason Foreman <jason@threeve.org>
2015-09-16 00:20:29 -0500

    An elegant weapon, for a more civilized age.
    Magit is not as clumsy or random as a terminal git.

--- init.el
+++ init.el
@@ -56,7 +56,23 @@
   :ensure t
   :config
   (evil-mode t)
+  (add-hook 'git-commit-mode-hook 'evil-insert-state)
   (define-key evil-normal-state-map ";" 'evil-ex))
+
+(use-package magit
+  :ensure t
+  :config
+  (setq magit-push-always-verify nil)
+  (define-key evil-normal-state-map (kbd "<SPC> g") 'magit-status))
+
+(use-package git-gutter
+  :ensure t
+  :config
+  (setq git-gutter:ask-p nil)
+  (define-key evil-normal-state-map "]h" 'git-gutter:next-hunk)
+  (define-key evil-normal-state-map "[h" 'git-gutter:previous-hunk)
+  (define-key evil-normal-state-map (kbd "<SPC> h s") 'git-gutter:stage-hunk)
+  (global-git-gutter-mode))
 
 (provide 'init)
 ;;; init.el ends here
