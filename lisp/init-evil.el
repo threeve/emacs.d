e40fa2de476b7464fc9c25d49bb1de61dd118766
Jason Foreman <jason@threeve.org>
2015-11-07 23:34:56 -0600

    Update evil config.
    - Add C-j/C-k bindings for scrolling.
    - Add additional evil extension libs:
      * commentary: port of vim-commentary for commenting regions
      * quickscope: adds some UI niceties for f/t/F/T commands
      * surround: port of vim-surround

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -10,9 +10,27 @@
 (use-package evil
   :ensure t
   :config
+  (use-package evil-commentary
+    :ensure t
+    :diminish ""
+    :config (evil-commentary-mode))
+  (use-package evil-quickscope
+    :ensure t
+    :config (global-evil-quickscope-mode t))
+  (use-package evil-surround
+    :ensure t
+    :config (global-evil-surround-mode t))
+  (setq evil-want-Y-yank-to-eol t)
   (evil-mode t)
   (add-hook 'git-commit-mode-hook 'evil-insert-state)
-  (define-key evil-normal-state-map ";" 'evil-ex))
+  (define-key evil-normal-state-map "j" 'evil-next-visual-line)
+  (define-key evil-normal-state-map "k" 'evil-previous-visual-line)
+  (define-key evil-normal-state-map (kbd "C-j") 'evil-scroll-down)
+  (define-key evil-normal-state-map (kbd "C-k") 'evil-scroll-up)
+  (define-key evil-motion-state-map (kbd "C-j") 'evil-scroll-down)
+  (define-key evil-motion-state-map (kbd "C-k") 'evil-scroll-up)
+  (define-key evil-normal-state-map ";" 'evil-ex)
+  (define-key evil-visual-state-map ";" 'evil-ex))
 
 (provide 'init-evil)
 ;; init-evil.el ends here
