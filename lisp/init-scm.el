a0aaf05b0bb7cc5a6c591fbb4c54a54af90961b8
Jason Foreman <jason@threeve.org>
2015-12-24 10:50:56 -0600

    Tweak magit key bindings.

--- lisp/init-scm.el
+++ lisp/init-scm.el
@@ -11,6 +11,12 @@
   :ensure t
   :config
   (setq magit-push-always-verify nil)
+  (evil-make-overriding-map magit-mode-map 'emacs)
+  (evil-define-key 'emacs magit-mode-map "j" #'magit-section-forward)
+  (evil-define-key 'emacs magit-mode-map "k" #'magit-section-backward)
+  ;; (evil-define-key 'emacs magit-mode-map "C-j" #'magit-section-forward-sibling) ; TODO: this doesn't work quite right...
+  ;; (evil-define-key 'emacs magit-mode-map "C-k" #'magit-section-backward-sibling)
+  (define-key magit-mode-map (kbd "C-w") 'evil-window-map)
   (define-key evil-normal-state-map (kbd "<SPC> g") 'magit-status))
 
 (use-package diff-hl
