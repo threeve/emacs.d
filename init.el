396360c7c60aa34b8df23c5a3e339f332ed1adec
Jason Foreman <jason@threeve.org>
2015-12-24 10:49:33 -0600

    Add melpa-stable package archive.

--- init.el
+++ init.el
@@ -5,7 +5,8 @@
 ;;; Code:
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
