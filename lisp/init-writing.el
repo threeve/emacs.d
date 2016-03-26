0c579272e201b6025592a739eb69a9b02c861bbb
Jason Foreman <jason@threeve.org>
2016-03-26 00:03:40 -0500

    Defer a couple packages to improve startup time.

--- lisp/init-writing.el
+++ lisp/init-writing.el
@@ -11,13 +11,15 @@
   (add-hook 'prog-mode-hook 'flyspell-prog-mode))
 
 (use-package writegood-mode
-  :ensure t)
+  :ensure t
+  :commands writegood-mode)
 
 (use-package langtool
   :ensure t
-  :config
+  :commands langtool-check
+  :init
   (setq langtool-language-tool-jar
-        "/usr/local/Cellar/languagetool/3.0/libexec/languagetool-commandline.jar"))
+        "/usr/local/Cellar/languagetool/3.2/libexec/languagetool-commandline.jar"))
 
 (provide 'init-writing)
-;; init-writing.el ends here
+;;; init-writing.el ends here
