0f149321ce453043eac3a7286c52776bb48df9c2
Jason Foreman <jason@threeve.org>
2016-03-25 21:23:46 -0500

    Tweak evil mode a bit.
    - Evil at the front of mode line
    - Hollow cursor in visual mode
    - Bindings for next/prev error
    - Dired-jump binding

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -6,9 +6,20 @@
 
 (use-package evil
   :ensure t
+  :demand t
+  :bind (:map evil-motion-state-map
+              ("j" . evil-next-visual-line)
+              ("k" . evil-previous-visual-line)
+              ("C-j" . evil-scroll-down)
+              ("C-k" . evil-scroll-up)
+              ("[ q" . previous-error)
+              ("] q" . next-error)
+              ("-" . dired-jump))
   :init
-  (setq evil-want-C-w-in-emacs-state t
-        evil-want-Y-yank-to-eol t)
+  (setq evil-mode-line-format '(before . mode-line-front-space)
+        evil-want-C-w-in-emacs-state t
+        evil-want-Y-yank-to-eol t
+        evil-visual-state-cursor 'hollow)
   :config
   (use-package evil-ediff :ensure t)
   (use-package evil-commentary
@@ -21,15 +32,9 @@
   (use-package evil-surround
     :ensure t
     :config (global-evil-surround-mode t))
-  (evil-mode t)
   (add-hook 'git-commit-mode-hook 'evil-insert-state)
-  (define-key evil-normal-state-map "j" 'evil-next-visual-line)
-  (define-key evil-normal-state-map "k" 'evil-previous-visual-line)
-  (define-key evil-normal-state-map (kbd "C-j") 'evil-scroll-down)
-  (define-key evil-normal-state-map (kbd "C-k") 'evil-scroll-up)
-  (define-key evil-motion-state-map (kbd "C-j") 'evil-scroll-down)
-  (define-key evil-motion-state-map (kbd "C-k") 'evil-scroll-up)
-  )
+  (evil-define-key 'normal dired-mode-map "-" 'dired-up-directory)
+  (evil-mode t))
 
 (provide 'init-evil)
 ;; init-evil.el ends here
