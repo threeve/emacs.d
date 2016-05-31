b831ce67a95d86458cf869c0b940ae3277769e6d
Jason Foreman <jason@threeve.org>
2016-05-30 21:23:34 -0500

    Set up package.el and use-package.

--- init.el
+++ init.el
@@ -1,2 +1,15 @@
+
+;; Set up package.el
+;; This needs to be in init.el before requiring config.org, otherwise
+;; it gets inserted in here automaticlly anyway.
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+
 ;; Load literate config file
+
 (org-babel-load-file "~/.emacs.d/config.org")
