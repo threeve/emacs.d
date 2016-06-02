6e8ac1afc33422c1106d51a5a14f0ab58e030dc0
Jason Foreman <jason@threeve.org>
2016-06-01 20:09:54 -0500

    WIP

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-quickscope evil-surround evil-commentary evil use-package))))
+    (pandoc-mode markdown-mode wgrep-ag ag fill-column-indicator clang-format projectile darkroom editorconfig ace-jump-mode fancy-narrow diff-hl git-messenger git-timemachine evil-magit idomenu smex ido-vertical-mode flx-ido ido-ubiquitous paradox yasnippet company flycheck org-projectile org-agenda-property orgit org-plus-contrib indent-guide which-key evil-quickscope evil-surround evil-commentary evil use-package)))
+ '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
